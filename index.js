ff2612d6fa51a247b179b0ece3c939a2d2114e51
gilbert <eleazarbiboum@gmail.com>
2022-03-14 21:53:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan";
+const height = 20;
+const message = `${name} is  ${height}`;
+module.exports = { name, height, message };
+console.log(name);
+console.log(message);
 
-module.exports = {name, height, message};
+
+
